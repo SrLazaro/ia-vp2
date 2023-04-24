efe57cb15c302fd2ec17792b292012ccad9e4a6b
Lázaro Sobreira <lazarosobreira@gmail.com>
2023-04-24 14:08:20 -0300

    ainda precisa ajeitar calculo da entropia

---
+++ Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+    public static double log(double base, double valor) {
+        return Math.log(valor) / Math.log(base);
+    }
+    public static double dividir(double dividendo, double divisor) {
+        return dividendo / divisor;
+    }
+}
--- Entropia.java
+++ Entropia.java
@@ -24,8 +24,41 @@ public class Entropia {
 
         ArrayList<Registro> registrosSumarizado = new ArrayList<Registro>();
         registrosSumarizado = sumarizarValoresAtributo(classe, investimentos);
+        double entropiaClasse = calcularValorEntropia(registrosSumarizado);
 
         System.out.println("até aqui tá funcionando kkk");
+
+    }
+
+    private double calcularValorEntropia(ArrayList<Registro> registrosSumarizado) {
+        
+        double valorEntropiaSumarizada = 0;
+        double fracao = 0;
+        double log = 0;
+        double valorEntropia = 0;
+
+        int totalRegistros = 0;
+
+        for (Registro registro : registrosSumarizado) {
+            totalRegistros += registro.getQuantidade();
+        }
+
+        try {
+            for (Registro registro : registrosSumarizado) {
+                
+                int quantidade = registro.getQuantidade();
+                fracao = Calculadora.dividir(quantidade, totalRegistros);
+                log = Calculadora.log(2, fracao);
+                valorEntropia = ( (fracao * -1) * log );
+    
+                valorEntropiaSumarizada -= valorEntropia;
+    
+            }  
+        } catch (Exception e) {
+            System.out.println("Erro ao realizar cálculo da Entropia");
+        }
+
+        return valorEntropiaSumarizada;
 
     }
 
